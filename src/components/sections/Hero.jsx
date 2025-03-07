5071127654d10dad534bfea3b76194d67d204c18
Vasudev <79050603+Velstruck@users.noreply.github.com>
2025-03-07 20:43:38 +0530

    added text animations

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -1,19 +1,29 @@
+import { AnimatedText } from "../ui/animated-underline-text-one"
 import { Button } from "../ui/button"
 import { RainbowButton } from "../ui/rainbow-button"
-import profileimage from "/profilepic.png"  
+import { Typewriter } from "../ui/typewriter-text"
+import profileimage from "/profilepic.png"
 
 const Hero = () => {
   return (
     <section className="min-h-[80vh] flex flex-col-reverse md:flex-row items-center justify-between px-4 md:px-8 lg:px-16 py-8 md:py-10 gap-8">
       <div className="flex-1 text-center md:text-left">
         <h1 className="text-4xl md:text-5xl lg:text-6xl font-bold mb-4">
-          Hi, I am Vasudev 🫸
+          <AnimatedText
+            text="Hi, I am Vasudev👋"
+            textClassName="text-5xl font-bold mb-2"
+            underlinePath="M 0,10 Q 75,0 150,10 Q 225,20 300,10"
+            underlineHoverPath="M 0,10 Q 75,20 150,10 Q 225,0 300,10"
+            underlineDuration={1.5}
+          />
         </h1>
         <p className="text-lg md:text-xl mb-6 text-muted-foreground max-w-2xl">
-          I try to write code that works🫡. 
-          <p>
-            A passionate software developer focused on creating intuitive and efficient web applications.          
-          </p>
+          <Typewriter
+                text={["A passionate developer", "focused on creating intuitive and efficient web applications."]}
+                speed={150}
+                loop={true}
+                className="text-xl"
+            />
         </p>
         <RainbowButton className="text-sm hover:scale-105 transition-all duration-300">
           <a href="https://drive.google.com/file/d/1t3StA1KYg1AryIekZmiIjiqhxWA4S50e/view?usp=sharing" target="_blank" rel="noopener noreferrer">
